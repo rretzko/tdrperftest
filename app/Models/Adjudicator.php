67e1786a808c87bad561e1110786f0d9f66ce92f
MFRHoldings <rick@mfrholdings.com>
2022-08-23 16:19:11 -0400

    Re-add testing throttled auditionees @ 75

--- app/Models/Adjudicator.php
+++ app/Models/Adjudicator.php
@@ -62,7 +62,7 @@ class Adjudicator extends Model
                 ->whereHas('instrumentations', function($query) use($roominstrumentation) {
                     $query->whereIn('id', [$roominstrumentation]);
                 })
-                    ->limit(60)
+                    ->limit(75)
                     ->get();
         }
 
