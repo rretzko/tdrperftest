4b1759117f741782f3bf932cb729a842318b1d6d
MFRHoldings <rick@mfrholdings.com>
2022-09-26 10:38:16 -0400

    Removed csrf verification from login form.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'https://https://sea-lion-app-uichk.ondigitalocean.app/login/tdr/update'
     ];
 }
